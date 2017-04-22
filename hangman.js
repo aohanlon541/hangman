6565853f50b5562188ba1258e76a566de5a5a5c9
aohanlon541 <allison.t.ohanlon@gmail.com>
2017-04-22 13:53:09 -0500

    GOT THE COUNTDOWN TO WORK

--- hangman.js
+++ hangman.js
@@ -9,31 +9,43 @@ for (var i = 0; i < alphabet.length; i++) {
 		btn.className = "buttonsAlphabet";
 		btn.appendChild(t);                                          
 		document.getElementById("buttonsContainer").appendChild(btn); 
+		btn.onclick = subtractTries;	
     }
 //To make a game of hangman, I need to have words that randomly selected
-
 var hangmanWords = ["pumpkin", "coffee", "pineapples", "chocolate"];
 var chosenWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
 //to make sure it words -- not necessary 
 console.log(chosenWord);
 console.log(chosenWord.length);
-
-// var currentWordLength = chosenWord.legnth
-
+//how many underscores a word needs
 var totalUnderscores = chosenWord.length;
 
 //Based on the chosen word, need to make a specifc amount of underscores ("_") for each character
-
 document.getElementById("currentWord").innerHTML = ("_ ".repeat(totalUnderscores));  
-
 // Need to establish number a tries somebody gets per game
-
 var tries = 10;
-var triesCountHtml = ("<p>" + tries + "left </p>");
-
 //print into HTML
+var triesCountHtml = ("<p>" + tries + " tries left </p>");
+	document.getElementById("triesCount").innerHTML = triesCountHtml;
+
+//function to subtract tries 
+
+function subtractTries(num) { 
+	if (tries > 0) {
+		tries--;	
+		console.log(tries);
+		var triesCountHtml = ("<p>" + tries + " tries left </p>");
+		document.getElementById("triesCount").innerHTML = triesCountHtml;
+	}
 	
-document.getElementById("triesCount").innerHTML = triesCountHtml;
+	else {
+		var triesCountHtml = ("<p> No tries left - Refresh to play again</p>");
+		document.getElementById("triesCount").innerHTML = triesCountHtml;
+	}	
+}
+
+// var onButtonClick = btn.onclick;
+subtractTries(btn.onclick);
 
 
 
