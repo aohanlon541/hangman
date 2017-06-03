36bad20399996104b83e2239088dd9b76f6d3e0b
Cody Childers <cody_k_childers@homedepot.com>
2017-06-03 13:38:47 -0500

    split drawLine into a func

--- hangman.js
+++ hangman.js
@@ -1,4 +1,4 @@
-//This will create my buttons 
+//This will create my buttons
 //Somehow I think I need to make them have different ids
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -7,7 +7,7 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 //To make a game of hangman, I need to have words that randomly selected
 var hangmanWords = ["tacos", "kombucha", "zilker", "dogs", "rainey", "bats", "hipster", "capit"];
 var chosenWord = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
-//to make sure it words -- not necessary 
+//to make sure it words -- not necessary
 console.log(chosenWord);
 //how many underscores a word needs
 var totalUnderscores = chosenWord.length;
@@ -22,124 +22,86 @@ var tries = 10;
 //print into HTML
 var youLoseHtml = ("<p>You lose!</p>");
 
-//function to subtract tries 
-			
- 
-function subtractTries() { 
-	if (tries > 0) {
-		tries--;	
-		console.log(tries);
-		var triesCountHtml = ("<p>" + tries + " tries left </p>");
-		document.getElementById("triesCount").innerHTML = triesCountHtml;
-		
-			if (tries === 8) {
-				lineOne();
-			}
-			else if (tries === 7) {
-				lineTwo();
-			}
-			else if (tries === 6) {
-				lineThree();
-				lineFour();
-			}
-			else if (tries === 5) {
-				circle();
-			}
-			else if (tries === 4) {
-				lineFive();
-			}
-			else if (tries === 3) {
-				lineSix();
-			}
-			else if (tries === 2) {
-				lineSeven();
-			}
-			else if (tries === 1) {
-				lineEight();
-			}
-			else if (tries === 0) {
-				lineNine();   
-			}
+//function to subtract tries
 
-	}
-	
-}
-
-subtractTries();
-
-// Stick Figure
-
-function lineOne() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(500, 145);
-  context.lineTo(200, 145);
-  context.stroke();
-  };
-function lineTwo() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(200, 145);
-  context.lineTo(200, 20);
-  context.stroke();
-  };
-function lineThree() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(200, 20);
-  context.lineTo(250, 20);
-  context.stroke();
-  };
-function lineFour() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(250, 20);
-  context.lineTo(250, 30);
-  context.stroke();
-  };
-function circle() {
+function drawCircle() {
 	var canvas = document.getElementById("hangmanBox");
 	var context = canvas.getContext("2d");context.beginPath();
 	context.beginPath();
 	context.arc(250,45,15,0,2*Math.PI);
 	context.stroke();
-	}
-function lineFive() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(250, 59);
-  context.lineTo(250, 100);
-  context.stroke();
-  };
+}
 
-function lineSix() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(250, 80);
-  context.lineTo(220, 60);
-  context.stroke();
-  };
-function lineSeven() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(250, 80);
-  context.lineTo(280, 60);
-  context.stroke();
-  };
-function lineEight() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(250, 100);
-  context.lineTo(220, 120);
-  context.stroke();
-  };
-function lineNine() {
-  var canvas = document.getElementById("hangmanBox");
-  var context = canvas.getContext("2d");context.beginPath();
-  context.moveTo(250, 100);
-  context.lineTo(280, 120);
-  context.stroke();
-  };
+function drawLine(start, finish) {
+	var canvas = document.getElementById("hangmanBox");
+	var context = canvas.getContext("2d");
+	context.beginPath();
+	context.moveTo(start[0], start[1]);
+	context.lineTo(finish[0], finish[1]);
+	context.stroke();
+};
+
+var LINE_MAPPING = {
+	1: {start: [500, 145], end: [200, 145]},
+	2: {start: [200, 145], end: [200, 20]},
+	3: {start: [200, 20], end: [250, 20]},
+	4: {start: [250, 20], end: [250, 30]},
+	5: {start: [250, 59], end: [250, 100]},
+	6: {start: [250, 80], end: [220, 60]},
+	7: {start: [250, 80], end: [280, 60]},
+	8: {start: [250, 100], end: [220, 120]},
+	9: {start: [250, 100], end: [280, 120]},
+}
+
+function executeDrawLine(line_number) {
+	var data = LINE_MAPPING[line_number];
+	var start = data.start;
+	var end = data.end;
+	drawLine(start, end);
+}
+
+
+function subtractTries() {
+	if (tries > 0) {
+		tries--;
+		console.log(tries);
+		var triesCountHtml = ("<p>" + tries + " tries left </p>");
+		document.getElementById("triesCount").innerHTML = triesCountHtml;
+
+			if (tries === 8) {
+				executeDrawLine(1);
+			}
+			else if (tries === 7) {
+				executeDrawLine(2);
+			}
+			else if (tries === 6) {
+				executeDrawLine(3);
+				executeDrawLine(4);
+			}
+			else if (tries === 5) {
+				drawCircle();
+			}
+			else if (tries === 4) {
+				executeDrawLine(5);
+			}
+			else if (tries === 3) {
+				executeDrawLine(6);
+			}
+			else if (tries === 2) {
+				executeDrawLine(7);
+			}
+			else if (tries === 1) {
+				executeDrawLine(8);
+			}
+			else if (tries === 0) {
+				executeDrawLine(9);
+			}
+
+	}
+
+}
+
+subtractTries();
 
 
 // var btnLetter = document.getElementsByClassName("buttonAbc");
@@ -149,8 +111,8 @@ function lineNine() {
 // 		console.log(letterPlacement);
 // 		 if (letterPlacement === -1) {
 // 		 	var spanLetter = document.createElement("span");
-// 		 	var tLetter = document.createTextNode([i] + ", "); 
-// 		 	spanA.appendChild(tLetter);  
+// 		 	var tLetter = document.createTextNode([i] + ", ");
+// 		 	spanA.appendChild(tLetter);
 // 		 	document.getElementById("letterCount").appendChild(spanA);
 // 		 }
 // 		 else {
@@ -178,11 +140,11 @@ btnA.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanA = document.createElement("span");
-	 	var tA = document.createTextNode("A, "); 
-	 	spanA.appendChild(tA);  
+	 	var tA = document.createTextNode("A, ");
+	 	spanA.appendChild(tA);
 	 	document.getElementById("letterCount").appendChild(spanA);
 	 	btnA.disable = true;
-	}	
+	}
 	 else {
 	 	// document.getElementById("currentWord").innerHTML = btn.value;
 	 	var textnodeA = document.createTextNode("A");
@@ -198,8 +160,8 @@ btnB.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanB = document.createElement("span");
-	 	var tB = document.createTextNode("B, "); 
-	 	spanB.appendChild(tB);  
+	 	var tB = document.createTextNode("B, ");
+	 	spanB.appendChild(tB);
 	 	document.getElementById("letterCount").appendChild(spanB);
 	 }
 	 else {
@@ -217,8 +179,8 @@ btnC.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanC = document.createElement("span");
-	 	var tC = document.createTextNode("C, "); 
-	 	spanC.appendChild(tC);  
+	 	var tC = document.createTextNode("C, ");
+	 	spanC.appendChild(tC);
 	 	document.getElementById("letterCount").appendChild(spanC);
 	 }
 	 else {
@@ -236,8 +198,8 @@ btnD.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanD = document.createElement("span");
-	 	var tD = document.createTextNode("D, "); 
-	 	spanD.appendChild(tD);  
+	 	var tD = document.createTextNode("D, ");
+	 	spanD.appendChild(tD);
 	 	document.getElementById("letterCount").appendChild(spanD);
 	 }
 	 else {
@@ -255,8 +217,8 @@ btnE.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanE = document.createElement("span");
-	 	var tE = document.createTextNode("E, "); 
-	 	spanE.appendChild(tE);  
+	 	var tE = document.createTextNode("E, ");
+	 	spanE.appendChild(tE);
 	 	document.getElementById("letterCount").appendChild(spanE);
 	 }
 	 else {
@@ -273,8 +235,8 @@ btnF.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanF = document.createElement("span");
-	 	var tF = document.createTextNode("F, "); 
-	 	spanF.appendChild(tF);  
+	 	var tF = document.createTextNode("F, ");
+	 	spanF.appendChild(tF);
 	 	document.getElementById("letterCount").appendChild(spanF);
 	 }
 	 else {
@@ -292,8 +254,8 @@ btnG.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanG = document.createElement("span");
-	 	var tG = document.createTextNode("G, "); 
-	 	spanG.appendChild(tG);  
+	 	var tG = document.createTextNode("G, ");
+	 	spanG.appendChild(tG);
 	 	document.getElementById("letterCount").appendChild(spanG);
 	 }
 	 else {
@@ -302,7 +264,7 @@ btnG.onclick = function printValue() {
 	 	var itemG = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemG.replaceChild(textnodeG, itemG.childNodes[0]);
 	 	}
-	} 
+	}
 //h
 var btnH = document.getElementById("btnH");
 btnH.onclick = function printValue() {
@@ -311,8 +273,8 @@ btnH.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanH = document.createElement("span");
-	 	var tH = document.createTextNode("H, "); 
-	 	spanH.appendChild(tH);  
+	 	var tH = document.createTextNode("H, ");
+	 	spanH.appendChild(tH);
 	 	document.getElementById("letterCount").appendChild(spanH);
 	 }
 	 else {
@@ -321,7 +283,7 @@ btnH.onclick = function printValue() {
 	 	var itemH = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemH.replaceChild(textnodeH, itemH.childNodes[0]);
 	 	}
-	} 
+	}
 //i
 var btnI = document.getElementById("btnI");
 btnI.onclick = function printValue() {
@@ -330,8 +292,8 @@ btnI.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanI = document.createElement("span");
-	 	var tI = document.createTextNode("I, "); 
-	 	spanI.appendChild(tI);  
+	 	var tI = document.createTextNode("I, ");
+	 	spanI.appendChild(tI);
 	 	document.getElementById("letterCount").appendChild(spanI);
 	 }
 	 else {
@@ -340,7 +302,7 @@ btnI.onclick = function printValue() {
 	 	var itemI = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemI.replaceChild(textnodeI, itemI.childNodes[0]);
 	 }
-	} 
+	}
 //j
 var btnJ = document.getElementById("btnJ");
 btnJ.onclick = function printValue() {
@@ -349,8 +311,8 @@ btnJ.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanJ = document.createElement("span");
-	 	var tJ = document.createTextNode("J, "); 
-	 	spanJ.appendChild(tJ);  
+	 	var tJ = document.createTextNode("J, ");
+	 	spanJ.appendChild(tJ);
 	 	document.getElementById("letterCount").appendChild(spanJ);
 	 }
 	 else {
@@ -359,7 +321,7 @@ btnJ.onclick = function printValue() {
 	 	var itemJ = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemJ.replaceChild(textnodeJ, itemJ.childNodes[0]);
 	 }
-	} 
+	}
 //K
 var btnK = document.getElementById("btnK");
 btnK.onclick = function printValue() {
@@ -368,8 +330,8 @@ btnK.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanK = document.createElement("span");
-	 	var tK = document.createTextNode("K, "); 
-	 	spanK.appendChild(tK);  
+	 	var tK = document.createTextNode("K, ");
+	 	spanK.appendChild(tK);
 	 	document.getElementById("letterCount").appendChild(spanK);
 	 }
 	 else {
@@ -378,7 +340,7 @@ btnK.onclick = function printValue() {
 	 	var itemK = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemK.replaceChild(textnodeK, itemK.childNodes[0]);
 	 }
-	} 
+	}
 //L
 var btnL = document.getElementById("btnL");
 btnL.onclick = function printValue() {
@@ -387,8 +349,8 @@ btnL.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanL = document.createElement("span");
-	 	var tL = document.createTextNode("L, "); 
-	 	spanL.appendChild(tL);  
+	 	var tL = document.createTextNode("L, ");
+	 	spanL.appendChild(tL);
 	 	document.getElementById("letterCount").appendChild(spanL);
 	 }
 	 else {
@@ -397,7 +359,7 @@ btnL.onclick = function printValue() {
 	 	var itemL = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemL.replaceChild(textnodeL, itemL.childNodes[0]);
 	 }
-	} 
+	}
 //m
 var btnM = document.getElementById("btnM");
 btnM.onclick = function printValue() {
@@ -406,8 +368,8 @@ btnM.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanM = document.createElement("span");
-	 	var tM = document.createTextNode("M, "); 
-	 	spanM.appendChild(tM);  
+	 	var tM = document.createTextNode("M, ");
+	 	spanM.appendChild(tM);
 	 	document.getElementById("letterCount").appendChild(spanM);
 	 }
 	 else {
@@ -416,7 +378,7 @@ btnM.onclick = function printValue() {
 	 	var itemM = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemM.replaceChild(textnodeM, itemM.childNodes[0]);
 	 }
-	} 
+	}
 //n
 var btnN = document.getElementById("btnN");
 btnN.onclick = function printValue() {
@@ -425,8 +387,8 @@ btnN.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanN = document.createElement("span");
-	 	var tN = document.createTextNode("N, "); 
-	 	spanN.appendChild(tN);  
+	 	var tN = document.createTextNode("N, ");
+	 	spanN.appendChild(tN);
 	 	document.getElementById("letterCount").appendChild(spanN);
 	 }
 	 else {
@@ -435,7 +397,7 @@ btnN.onclick = function printValue() {
 	 	var itemN = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemN.replaceChild(textnodeN, itemN.childNodes[0]);
 	 }
-	} 
+	}
 //o
 var btnO = document.getElementById("btnO");
 btnO.onclick = function printValue() {
@@ -444,8 +406,8 @@ btnO.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanO = document.createElement("span");
-	 	var tO = document.createTextNode("O, "); 
-	 	spanO.appendChild(tO);  
+	 	var tO = document.createTextNode("O, ");
+	 	spanO.appendChild(tO);
 	 	document.getElementById("letterCount").appendChild(spanO);
 	 }
 	 else {
@@ -463,8 +425,8 @@ btnP.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanP = document.createElement("span");
-	 	var tP = document.createTextNode("P, "); 
-	 	spanP.appendChild(tP);  
+	 	var tP = document.createTextNode("P, ");
+	 	spanP.appendChild(tP);
 	 	document.getElementById("letterCount").appendChild(spanP);
 	 }
 	 else {
@@ -473,7 +435,7 @@ btnP.onclick = function printValue() {
 	 	var itemP = document.getElementById("currentWord").childNodes[letterPlacement];
 	 	itemP.replaceChild(textnodeP, itemP.childNodes[0]);
 	 }
-	} 
+	}
 //q
 var btnQ = document.getElementById("btnQ");
 btnQ.onclick = function printValue() {
@@ -482,8 +444,8 @@ btnQ.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanQ = document.createElement("span");
-	 	var tQ = document.createTextNode("Q, "); 
-	 	spanQ.appendChild(tQ);  
+	 	var tQ = document.createTextNode("Q, ");
+	 	spanQ.appendChild(tQ);
 	 	document.getElementById("letterCount").appendChild(spanQ);
 	 }
 	 else {
@@ -501,8 +463,8 @@ btnR.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanR = document.createElement("span");
-	 	var tR = document.createTextNode("R, "); 
-	 	spanR.appendChild(tR);  
+	 	var tR = document.createTextNode("R, ");
+	 	spanR.appendChild(tR);
 	 	document.getElementById("letterCount").appendChild(spanR);
 	 }
 	 else {
@@ -520,8 +482,8 @@ btnS.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanS = document.createElement("span");
-	 	var tS = document.createTextNode("S, "); 
-	 	spanS.appendChild(tS);  
+	 	var tS = document.createTextNode("S, ");
+	 	spanS.appendChild(tS);
 	 	document.getElementById("letterCount").appendChild(spanS);
 	 }
 	 else {
@@ -539,8 +501,8 @@ btnT.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanT = document.createElement("span");
-	 	var tT = document.createTextNode("T, "); 
-	 	spanT.appendChild(tT);  
+	 	var tT = document.createTextNode("T, ");
+	 	spanT.appendChild(tT);
 	 	document.getElementById("letterCount").appendChild(spanT);
 	 }
 	 else {
@@ -558,8 +520,8 @@ btnU.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanU = document.createElement("span");
-	 	var tU = document.createTextNode("U, "); 
-	 	spanU.appendChild(tU); 
+	 	var tU = document.createTextNode("U, ");
+	 	spanU.appendChild(tU);
 	 	document.getElementById("letterCount").appendChild(spanU);
 	 }
 	 else {
@@ -577,8 +539,8 @@ btnV.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanV = document.createElement("span");
-	 	var tV = document.createTextNode("V, "); 
-	 	spanV.appendChild(tV); 
+	 	var tV = document.createTextNode("V, ");
+	 	spanV.appendChild(tV);
 	 	document.getElementById("letterCount").appendChild(spanV);
 	 }
 	 else {
@@ -596,8 +558,8 @@ btnW.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanW = document.createElement("span");
-	 	var tW = document.createTextNode("W, "); 
-	 	spanW.appendChild(tW); 
+	 	var tW = document.createTextNode("W, ");
+	 	spanW.appendChild(tW);
 	 	document.getElementById("letterCount").appendChild(spanW);
 	 }
 	 else {
@@ -615,8 +577,8 @@ btnX.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanX = document.createElement("span");
-	 	var tX = document.createTextNode("X, "); 
-	 	spanX.appendChild(tX); 
+	 	var tX = document.createTextNode("X, ");
+	 	spanX.appendChild(tX);
 	 	document.getElementById("letterCount").appendChild(spanX);
 	 }
 	 else {
@@ -634,8 +596,8 @@ btnY.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanY = document.createElement("span");
-	 	var tY = document.createTextNode("Y, "); 
-	 	spanY.appendChild(tY); 
+	 	var tY = document.createTextNode("Y, ");
+	 	spanY.appendChild(tY);
 	 	document.getElementById("letterCount").appendChild(spanY);
 	 }
 	 else {
@@ -653,8 +615,8 @@ btnZ.onclick = function printValue() {
 	 if (letterPlacement === -1) {
 	 	subtractTries();
 	 	var spanZ = document.createElement("span");
-	 	var tZ = document.createTextNode("Z, "); 
-	 	spanZ.appendChild(tZ);  
+	 	var tZ = document.createTextNode("Z, ");
+	 	spanZ.appendChild(tZ);
 	 	document.getElementById("letterCount").appendChild(spanZ);
 	 }
 	 else {
@@ -669,7 +631,7 @@ btnZ.onclick = function printValue() {
 
 // When a letter is pressed it needs disappear & count against the guesses
 
-     
 
-// When the game is over (either by too many tries or winning), need to change 
+
+// When the game is over (either by too many tries or winning), need to change
 // the hangman to "you win!" or "you lose!"
